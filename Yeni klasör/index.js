f40de76722c9c9aed21cdc322bc59a6c5c42d78f
Sanubar-Basirzada <7bqlyhs@code.edu.az>
2023-04-25 13:36:49 +0400

    dsad

---
+++ Yeni klasör/index.js
@@ -0,0 +1,15 @@
+// const icon = document.querySelector(".icon")
+// const sen = document.querySelector(".sen")
+// icon.addEventListener("click", function(){
+//     // sen.style.marginLeft = "5%"
+    
+//     sen.classList.toggle("men")
+// })
+
+// imgContainer.addEventListener('click', function() {
+//     
+//     lorem.style.color = lorem.style.color ? 'white' : 'blue';
+// });
+
+
+///////////////////////////
---
+++ mastercard/script.js
@@ -0,0 +1,9 @@
+const body1 = document.getElementById("body1")
+const setting = document.getElementById("setting")
+const cardid = document.getElementById("cardid")
+setting.addEventListener("click",function(){
+   body1.style.backgroundColor = "black"
+   cardid.style.backgroundColor = "pink"
+   cardid.style.color = "hotpink"
+    
+})
