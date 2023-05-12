0b70e93f67dc99b68224becf2ac5bc479ee27e49
Sanubar-Basirzada <7bqlyhs@code.edu.az>
2023-05-12 11:37:22 +0400

    adsasd

--- 24-AgencyHomePage/assets/js/script.js
+++ 24-AgencyHomePage/assets/js/script.js
@@ -65,3 +65,50 @@ formap.addEventListener("submit",function (e){
     console.log(JSON.parse(localStorage.getItem("arr")));
     e.preventDefault();
 })
+
+
+
+
+// fetch("")
+// .then(response=>response.json())
+// .then(data=> {
+//     for(let i=0; i<data.lenght; i++){
+//         for(let index = 0; index <adlar; index++){
+//             if(i==index) {
+//                 adlar[i].innerText=data[index].name.common;
+//             }
+//         }
+//     }
+// });
+
+function cardInformation(data) {
+    const topone = document.createElement("div");
+    const ptop = document.createElement("p");
+    // const ptop1 = document.createElement("p");
+    // const ptop2 = document.createElement("p");
+    // const img=document.createElement("img");
+    topone.classList.add('box')
+    // img.classList.add( 'grahimg')
+    ptop.classList.add('text');
+    // ptop1.classList.add('martip');
+    // ptop2.classList.add('metap');
+    ptop.innerText =  `${data.name}` 
+    // ptop1.innerText = `${data.species}`
+    // ptop2.innerText = `${data.species}`
+    topone.appendChild(ptop);
+    // topone.appendChild(ptop1);
+    // topone.appendChild(ptop2);
+    leftestnewone1.appendChild(topone);
+    };
+  
+  
+  const leftestnewone1 = document.getElementById("leftestnewone1");
+  fetch("https://rickandmortyapi.com/api/character")
+    .then(response => response.json())
+    .then(data => {
+      
+      cardInformation(data.results[0]);
+      cardInformation(data.results[1]);
+      cardInformation(data.results[2]);
+  
+    })
